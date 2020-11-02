d1df8c88c378da437eceffec1d98ca6f2ae5e8ca
DESKTOP-SH1CGI8\Sebastian <sebastian.cadavid9412@gmail.com>
2020-11-02 10:36:13 -0500

    style: class nodoSimple

--- lab-1-2.py
+++ lab-1-2.py
@@ -5,6 +5,60 @@ Created on Mon Nov  2 09:13:25 2020
 @author: Sebastian
 """
 
-archivo = open("prueba.txt")
 
-print(archivo.read())
+class Automata:
+    def __init__(self,cadena):
+        self.cadena=cadena
+        
+    
+    
+    def leerArchivo(self):
+        with open('prueba.txt', 'r') as f:
+            for linea in f:
+                linea=f.readline()
+                
+                
+                
+                
+
+
+
+class listaSimpleLigada:
+    def __init__(self,linea1):
+        self.linea1=linea1
+
+
+class nodoSimple:
+    /*constructor*/
+    def __init__(self,clase,valor,posicionMemoria,liga):
+        self.clase=clase
+        self.valor=valor 
+        self.posicionMemoria=posicionMemoria
+        self.liga=liga
+    
+    def retornaClase():
+        return clase;
+    def retornaValor():
+        return valor;
+    def retornaNodoSimple():
+        return nodoSimple;
+    def retornPosicionMemoria():
+        return posicionMemoria;
+    
+    def asignaClase(tipo):
+        clase=tipo
+    def asignaValor(tipo):
+        valor=tipo
+    def asignaNodoSimple(tipo):
+        nodoSimple=tipo
+    def asignaPosicionMemoria(tipo):
+        posicionMemoria=tipo
+    
+    
+    
+    
+    
+    
+    
+            
+        
